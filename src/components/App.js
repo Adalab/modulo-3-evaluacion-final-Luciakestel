ee884ab422583a576b4aba8aa11881234b75a317
Luciakestel <luciakestelpires@gmail.com>
2022-12-12 15:44:34 +0100

    Add api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import getDataFromApi from '../services/api';
 import '../styles/App.scss';
 
 function App() {
 
   //VARIABLES ESTADO
+const [characters, setCharacters] = useState({});
+
   //USEEFFECT
+  useEffect(() => {
+    getDataFromApi().then((data) => setCharacters(data));
+  }, []);
+
   //FUNCIONES HANDLER
   //FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR EL HTML
   //HTML EN EL RETURN
---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+const getDataFromApi = () => {
+    return fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => {
+        const characters = data.results.map((eachCharacter) => {
+          const character = {
+                id: eachCharacter.id,
+                name: eachCharacter.name,
+                species: eachCharacter.species,
+                status: eachCharacter.status,
+                gender: eachCharacter.gender,
+                image: eachCharacter.image,
+                origin: eachCharacter.origin.name,
+          };
+          return character;
+        });
+        return characters;
+      });
+  };
+  
+  export default getDataFromApi;
